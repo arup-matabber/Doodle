e42aa6a06d5f4b73a9a8f085bf53eb833de4c4ba
Arup Matabber <arupmatabber03@gmail.com>
2026-08-07 20:24:44 +0530

    register escrow instructions

---
+++ programs/escrow/src/instructions/mod.rs
@@ -0,0 +1,19 @@
+pub mod approve_milestone;
+pub mod cancel_before_funding;
+pub mod create_milestone;
+pub mod fund_milestone;
+pub mod full_timeout_release;
+pub mod partial_timeout_release;
+pub mod rate_freelancer;
+pub mod settle_reputation;
+pub mod submit_delivery;
+
+pub use approve_milestone::*;
+pub use cancel_before_funding::*;
+pub use create_milestone::*;
+pub use fund_milestone::*;
+pub use full_timeout_release::*;
+pub use partial_timeout_release::*;
+pub use rate_freelancer::*;
+pub use settle_reputation::*;
+pub use submit_delivery::*;
